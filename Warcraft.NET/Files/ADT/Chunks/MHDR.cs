1baa66450f04c8fa62906fbcf73a423fa0b685b7
Arektor <maxence@symfonye.com>
2026-04-29 20:50:27 +0200

    Offsets in MHDR are unsigned ints

--- Warcraft.NET/Files/ADT/Chunks/MHDR.cs
+++ Warcraft.NET/Files/ADT/Chunks/MHDR.cs
@@ -2,6 +2,7 @@
 using Warcraft.NET.Files.Interfaces;
 using System.IO;
 using Warcraft.NET.Attribute;
+using System;
 
 namespace Warcraft.NET.Files.ADT.Chunks
 {
@@ -24,57 +25,57 @@ namespace Warcraft.NET.Files.ADT.Chunks
         /// <summary>
         /// Gets or sets offset into the file where the MCIN Chunk can be found.
         /// </summary>
-        public int MCINOffset { get; set; } = 0;
+        public uint MCINOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MTEX Chunk can be found.
         /// </summary>
-        public int MTEXOffset { get; set; } = 0;
+        public uint MTEXOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MMDX Chunk can be found.
         /// </summary>
-        public int MMDXOffset { get; set; } = 0;
+        public uint MMDXOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MMID Chunk can be found.
         /// </summary>
-        public int MMIDOffset { get; set; } = 0;
+        public uint MMIDOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MWMO Chunk can be found.
         /// </summary>
-        public int MWMOOffset { get; set; } = 0;
+        public uint MWMOOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MWID Chunk can be found.
         /// </summary>
-        public int MWIDOffset { get; set; } = 0;
+        public uint MWIDOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MMDF Chunk can be found.
         /// </summary>
-        public int MDDFOffset { get; set; } = 0;
+        public uint MDDFOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MODF Chunk can be found.
         /// </summary>
-        public int MODFOffset { get; set; } = 0;
+        public uint MODFOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MFBO Chunk can be found. This is only set if the Flags contains MDHR_MFBO.
         /// </summary>
-        public int MFBOOffset { get; set; } = 0;
+        public uint MFBOOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MH2O Chunk can be found.
         /// </summary>
-        public int MH2OOffset { get; set; } = 0;
+        public uint MH2OOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets offset into the file where the MTXF Chunk can be found.
         /// </summary>
-        public int MTXFOffset { get; set; } = 0;
+        public uint MTXFOffset { get; set; } = 0;
 
         /// <summary>
         /// Gets or sets mamp value (Cata+)
@@ -135,21 +136,21 @@ namespace Warcraft.NET.Files.ADT.Chunks
             {
                 Flags = (MHDRFlags)br.ReadInt32();
 
-                MCINOffset = br.ReadInt32();
-                MTEXOffset = br.ReadInt32();
+                MCINOffset = br.ReadUInt32();
+                MTEXOffset = br.ReadUInt32();
 
-                MMDXOffset = br.ReadInt32();
-                MMIDOffset = br.ReadInt32();
+                MMDXOffset = br.ReadUInt32();
+                MMIDOffset = br.ReadUInt32();
 
-                MWMOOffset = br.ReadInt32();
-                MWIDOffset = br.ReadInt32();
+                MWMOOffset = br.ReadUInt32();
+                MWIDOffset = br.ReadUInt32();
 
-                MDDFOffset = br.ReadInt32();
-                MODFOffset = br.ReadInt32();
+                MDDFOffset = br.ReadUInt32();
+                MODFOffset = br.ReadUInt32();
 
-                MFBOOffset = br.ReadInt32();
-                MH2OOffset = br.ReadInt32();
-                MTXFOffset = br.ReadInt32();
+                MFBOOffset = br.ReadUInt32();
+                MH2OOffset = br.ReadUInt32();
+                MTXFOffset = br.ReadUInt32();
 
                 MAMPValue = br.ReadByte();
 
